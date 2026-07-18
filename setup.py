a98e1bc01222dfc1921879d3968e6fa62a5d4f86
Saurob17 <mdsaurobbhuyan@gmail.com>
2026-07-18 22:25:49 +0600

    requerments complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa_approval_prediction",
+    version="0.1.0",
+    author="Md Saurob Bhuyan",
+    author_email="mdsaurobbhuyan@gmail.com",
+    packages=find_packages(),
+)
